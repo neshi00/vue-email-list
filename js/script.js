ade3da77a900461e95fbed41aa25d1ee515507b2
neshi00 <luca.ascione85@gmail.com>
2022-01-24 15:25:20 +0100

    esercizio base

---
+++ js/script.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: "#app",
+  data: {
+    emails: [],
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+          this.emails.push(result.data.response);
+        });
+    }
+  },
+});
